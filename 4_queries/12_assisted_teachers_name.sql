5ca3aea4c73f9c6d204a56e09f8e9adf5b8285bd
Josiah <josiah.pilon@yahoo.ca>
2021-04-05 04:09:02 +0000

    Make queries with node-postgres

--- 4_queries/12_assisted_teachers_name.sql
+++ 4_queries/12_assisted_teachers_name.sql
@@ -5,10 +5,10 @@
 -- Order by the instructor's name.
 -- This query needs to select data for a cohort with a specific name, use 'JUL02' for the cohort's name here.
 
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-FROM assistance_requests
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-JOIN teachers ON teachers.id = teacher_id
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
-ORDER BY teachers.name;
+ORDER BY teacher;
